744c51e818563efd97c40cf90a071b80a780ba83
xnarlo <narlolinco@gmail.com>
2025-03-02 19:40:41 +0800

    separate serial.js to initialize once

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-<<<<<<< HEAD
 const { serialPort } = require('./serial');
-=======
->>>>>>> 732ba5e3f7ee132f57616276bb5b40999bc6b4b0
 
 const app = express();
 const port = 3000;
@@ -15,7 +12,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const smsRoutes = require('./routes/sms');
 const callRoutes = require('./routes/call');
-<<<<<<< HEAD
 
 app.use('/sms', smsRoutes);
 app.use('/call', callRoutes);
@@ -27,11 +23,6 @@ app.get('/sendsmspage', (req, res) => {
 app.get('/callpage', (req, res) => {
   res.redirect('/call/callpage');
 });
-=======
-
-app.use('/sms', smsRoutes);
-app.use('/call', callRoutes);
->>>>>>> 732ba5e3f7ee132f57616276bb5b40999bc6b4b0
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
